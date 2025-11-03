e294cb5e6bda4f2d5f2ee0b55ba4814cf2b97acf
Ricardo Vieira <devabefarma3@outlook.com>
2025-11-03 11:12:45 -0300

    rota corrigida

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -31,7 +31,7 @@ function injectPolyfillPlugin(): Plugin {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/webapp',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
